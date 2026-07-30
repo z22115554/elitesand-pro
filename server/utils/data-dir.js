01bb9e5e14add0f22314cf410eb3a5c0d096c1b0
z22115554 <z22115554@gmail.com>
2026-07-30 18:56:19 +0800

    site: publish Elitesand Pro landing page

--- server/utils/data-dir.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Compatibility export for modules added before app-paths.js. New code should
-// import from app-paths directly so every writable location has one authority.
-const { dataDir, projectRoot } = require('./app-paths');
-
-module.exports = { dataDir, projectRoot };
